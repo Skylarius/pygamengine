0784093f140653888945d4aaaa041f20bd3efd4d
Ilario Gabriele Gerloni <ilario.gerloni@gmail.com>
2024-12-18 14:20:26 +0000

    ui: add font size

--- games/test_ui.py
+++ games/test_ui.py
@@ -105,20 +105,21 @@ if __name__ == "__main__":
     Ngine.create_new_gameobject(Text("txt_tr", bigpanel.get_position_with_anchor(Anchor.TOP_RIGHT), text="TR"))
     Ngine.create_new_gameobject(Text("txt_bl", bigpanel.get_position_with_anchor(Anchor.BOTTOM_LEFT), text="BL"))
     Ngine.create_new_gameobject(Text("txt_br", bigpanel.get_position_with_anchor(Anchor.BOTTOM_RIGHT), text="BR"))
-    Ngine.create_new_gameobject(Text("txt_top_left", bigpanel.transform.get_position(), text="*TopLeft", anchor=Anchor.TOP_LEFT))
-    Ngine.create_new_gameobject(Text("txt_top_right", bigpanel.transform.get_position(), text="TopRight*", anchor=Anchor.TOP_RIGHT))
-    Ngine.create_new_gameobject(Text("txt_bottom_left", bigpanel.transform.get_position(), text="_BottomLeft", anchor=Anchor.BOTTOM_LEFT))
-    Ngine.create_new_gameobject(Text("txt_bottom_right", bigpanel.transform.get_position(), text="BottomRight_", anchor=Anchor.BOTTOM_RIGHT))
+    Ngine.create_new_gameobject(Text("txt_top_left", bigpanel.transform.get_position(), text="*TopLeft", anchor=Anchor.TOP_LEFT, font_size=16))
+    Ngine.create_new_gameobject(Text("txt_top_right", bigpanel.transform.get_position(), text="TopRight*", anchor=Anchor.TOP_RIGHT, font_size=16))
+    Ngine.create_new_gameobject(Text("txt_bottom_left", bigpanel.transform.get_position(), text="_BottomLeft", anchor=Anchor.BOTTOM_LEFT,font_size=16))
+    Ngine.create_new_gameobject(Text("txt_bottom_right", bigpanel.transform.get_position(), text="BottomRight_", anchor=Anchor.BOTTOM_RIGHT, font_size=16))
 
     # Create Slider
     slider_horizontal = Slider("slider_h", Transform.get_vectors_sum(moving_button.get_position(),(0, moving_button.height + 30)),
-        Anchor.TOP_LEFT, start_value=15.5, max_value=14, min_value=12, step=0.1
+        Anchor.TOP_LEFT, start_value=12.5, max_value=14, min_value=9, step=0.1
     )
 
     Ngine.create_new_gameobject(slider_horizontal)   
 
     def change_slider_horizontal_text(new_value: float):
         text_slider_horizontal.set_update(f"Slider: {new_value:.2f}")
+        text_slider_horizontal.set_font_size(int(new_value)*2)
     
     slider_horizontal.on_slider_change = change_slider_horizontal_text
 
