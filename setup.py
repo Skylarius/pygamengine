7e919dccf79d237a31a96e10d4a751b5e1afb77e
Ilario Gabriele Gerloni <ilario.gerloni@gmail.com>
2025-01-02 15:03:27 +0000

    update setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [
 
 setup(
     name='pygamengine',
-    version='0.2.0',
+    version='0.2.1',
     description='Game Engine based on PyGame',
     long_description=readme,
     author='Ilario Gerloni',
